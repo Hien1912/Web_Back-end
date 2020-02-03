70f60991f2f828ac2dcb13ae4c6d90cbb69bc301
Vinh Hiển <hienvh00@gmail.com>
2020-02-03 10:55:21 +0700

    [Thực hành] Ứng dụng hiển thị thời gian của server

---
+++ Thuc_Hanh/Server_Time/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>[Thực hành] Ứng dụng hiển thị thời gian của server</title>
+</head>
+<body>
+<h1> Bây giờ là: <?= date('D-d-M-Y h:m:s'); ?> </h1>
+</body>
+</html>
