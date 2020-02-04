4dbe5ae3f9a4f645e204ec19fb0957aa77b94ac7
Vinh Hiển <hienvh00@gmail.com>
2020-02-04 13:41:39 +0700

    [Bài tập] Ứng dụng Product Discount Calculator

--- Bai_Tap/Overview_Web/Discount_Calculator/index.php
+++ Bai_Tap/Overview_Web/Discount_Calculator/index.php
@@ -8,44 +8,20 @@
     <title>[Bài tập] Ứng dụng Product Discount Calculator</title>
 </head>
 <body>
-    <?php
-    $percentErr = $priceErr = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $pri = $_POST["price"];
-        $dis = $_POST["discount_percent"];
-        if (empty($pri) || empty($dis)) {
-            if (empty($pri)) {
-                $priceErr = "Price is required";
-            }
-            if (empty($dis)) {
-                $percentErr = "Discount Percent is required";
-            }
-        } else {
-            require "process.php";
-        }
-    }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return (int) $data;
-    }
-    ?>
     <div id="content">
         <h1>Product Discount Calculator</h1>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <p class="error">* is required</p>
+        <form method="post" action="process.php">
             <div id="data">
                 <label>Product Description:</label>
                 <input type="text" name="description"><br>
                 <label>List Price:</label>
                 <input type="text" name="price">
-                <span class="error">*<?php echo $priceErr;?></span>
+                <span class="error">*<?php echo $priceErr ?? ""; ?></span>
                 <br>
                 <label>Discount Percent:</label>
                 <input type="text" name="discount_percent"> (%)
-                <span class="error">*<?php echo $percentErr;?></span>
+                <span class="error">*<?php echo $percentErr ?? ""; ?></span>
                 <br>
             </div>
             <div id="buttons">
--- Bai_Tap/Overview_Web/Discount_Calculator/process.php
+++ Bai_Tap/Overview_Web/Discount_Calculator/process.php
@@ -1,7 +1,38 @@
 <?php
-$description = test_input($_POST["description"]);
-$price = test_input($_POST["price"]);
-$percent = test_input($_POST["discount_percent"]);
-$amount=$price*$percent/100;
-$discount=$description - $amount;
-require "display.php";
+$percentErr = $priceErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pri = $_POST["price"];
+    $dis = $_POST["discount_percent"];
+    if (empty($pri) || empty($dis)||!is_numeric($pri)||!is_numeric($dis)) {
+        if (empty($pri)) {
+            $priceErr = "Price is required";
+        }
+        if (empty($dis)) {
+            $percentErr = "Discount Percent is required";
+        }
+        if (!is_numeric($pri)) {
+            $priceErr = "Price is not number";
+        }
+        if (!is_numeric($dis)) {
+            $percentErr = "Discount Percent is not number";
+        }
+        require "index.php";
+    }
+    else {
+        $description = test_input($_POST["description"]);
+        $price = test_input($_POST["price"]);
+        $percent = test_input($_POST["discount_percent"]);
+        $amount=$price*$percent/100;
+        $discount=$price - $amount;
+        require "display.php";
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return (int) $data;
+}
+?>
