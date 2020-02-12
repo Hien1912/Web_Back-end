a73d25b58cfa2ad3cb267dfdfe380c4687be0c4a
TieuNhiKk <hienvh00@gmail.com>
2020-02-12 11:37:28 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ Bai_Tap/Inheritance/Point2D_Point3D/Point3D.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class Point3D extends Point2D{
+    protected float $y;
+
+    public function __construct(float $x = 0.0 , float $y = 0.0 , float $z = 0.0)
+    {
+        parent::__construct($x,$y);
+        $this->z = $z;
+    }
+
+    public function setZ(float $z)
+    {
+        $this->z = $z;
+    }
+
+    public function getZ()
+    {
+        return $this->z;
+    }
+
+    public function setXYZ(float $x, float $y, float $z)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+
+    public function getXY()
+    {
+        return [$this->x , $this->y , $this->z];
+    }
+
+    public function __toString()
+    {
+        return "This is class Point3D: <br>
+                x = $this->x <br>
+                y = $this->y <br>
+                z = $this->z <br>";
+    }
+}
+
+?>
