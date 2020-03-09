84bc6d951b47dfe0b333ec2e7c8f9ce05b88cbd0
Vinh Hiển <hienvh00@gmail.com>
2020-03-09 13:49:25 +0700

    [Thực hành] Ứng dụng xem giờ hiện tại của các thành phố trên thế giới

--- GoFood/routes/web.php
+++ GoFood/routes/web.php
@@ -14,6 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+
+
+
+
+
+
+
+Route::get('/time', function () {
+    return view('index');
+});
+
+Route::get('/time/{timezone?}', function ($timezone = null) {
+    if (!empty($timezone)) {
+        // Khởi tạo giá trị giờ theo múi giờ UTC
+        $time = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone('UTC'));
+
+        // Thay đổi về múi giờ được chọn
+        $time->setTimezone(new DateTimeZone(str_replace('-', '/', $timezone)));
+
+        // Hiển thị giờ theo định dạng mong muốn
+        echo 'Múi giờ bạn chọn ' . $timezone . ' hiện tại đang là: ' . $time->format('d-m-Y H:i:s');
+    }
+    return view('index');
+});
+
 Route::get('/home', 'HomeController@index');
 
 Route::get('/user-name/{name?}', function ($name = 'Sohel') {
@@ -62,9 +90,9 @@ Route::post('/dictionary', function () {
     return view('translate', ['result' => "Không tìm thấy " . $word, 'word' => $word]);
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('test', function () {
     $haha = env('APP_NAME');
